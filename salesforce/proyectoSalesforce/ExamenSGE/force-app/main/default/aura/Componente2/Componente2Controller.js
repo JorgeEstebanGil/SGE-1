b9ddf591750b42b8e1b8f2a7dbbf2d519951c18e
JorgeEstebanGil <125263700+JorgeEstebanGil@users.noreply.github.com>
2024-02-19 10:15:37 +0100

    otro commit por aqui...
    algo atascado

--- salesforce/proyectoSalesforce/ExamenSGE/force-app/main/default/aura/Componente2/Componente2Controller.js
+++ salesforce/proyectoSalesforce/ExamenSGE/force-app/main/default/aura/Componente2/Componente2Controller.js
@@ -2,6 +2,18 @@
     myAction : function(component, event, helper) {
 
     },
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getSavedLists");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.savedLists", response.getReturnValue());
+            } else {
+                // Manejar el error
+            }
+        });
+        $A.enqueueAction(action);
+    },
     addToList : function(component, event) {
         if (component.get("v.isRecording")) { // changed from "isRecording" to "v.isRecording"
             var lista = component.get("v.lista");
@@ -14,10 +26,20 @@
         component.set("v.lista", []);
     },
     Grabar : function(component, event, helper) {
-        component.set("v.lista", []);
+        component.set("v.Lista", [])
         component.set("v.isRecording", true);
     },
     Stop : function(component, event, helper) {
         component.set("v.isRecording", false);
+    },
+    Guardar : function(component, event, helper) {
+        var action = component.get("c.saveList");
+        var lista = component.get("v.lista");
+        action.setParams({
+            "numbers" : lista
+        });
+        component.set("v.Lista", [])
+        $A.enqueueAction(action);
     }
+    
 })
